1a7f2804a5522ad3fe83b5bc98f6836a069527b2
hanetyan <hanedango2@gmail.com>
2024-01-09 14:48:02 +0900

    fifth commit

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4433", "message": "Adoと.tukiとカラオケ行きたい"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4433", "message": "Adoと.tukiと結婚したい"})
     }
 }
